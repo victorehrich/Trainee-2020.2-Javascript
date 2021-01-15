6c85134f67ccbc4d5ab001e871a952a78ef2b80e
Victor.Ehrich <victorehrich@gmail.com>
2021-01-15 20:12:47 -0300

    o comeco

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+function mostrarQuestao() {
+    document.getElementById("confirmar")
+        .addEventListener("click", function() {
+  document.getElementById("listaRespostas").hidden = true;
+  document.getElementById("listaRespostas").hidden = false;
+}, false);
+
+     
+
+}
+
+function finalizarQuiz() {
+    
+}
