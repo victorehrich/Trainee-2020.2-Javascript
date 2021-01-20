0527f5828fccd2e831f011ec9a6eefa4e0ad95d1
Victor.Ehrich <victorehrich@gmail.com>
2021-01-20 18:21:45 -0300

    teste

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 
+var requestURL = "https://quiz-trainee.herokuapp.com/questions";
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+var forme = request.response;
+var x=0;
 
 function mostrarQuestao() {
+  document.getElementById("listaRespostas").style.display = "block";
   document.getElementById("confirmar").innerHTML = "Próxima";
-  document.getElementById("listaRespostas").set;
+  x=x+1;
+
+  var resp = jsonObj['options'];
+
+  if(x==1){
+    document.getElementsByName("titulo")[0].textContent = 
+    document.getElementsByName("resposta")[0].span.textContent = resp[0]
+  }
+  
+
+
 }
 
 function finalizarQuiz() {
