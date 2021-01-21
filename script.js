f6677cebecbd4da28c085922884b1201f1e75d25
Victor.Ehrich <victorehrich@gmail.com>
2021-01-21 01:32:16 -0300

    deu bom!

--- script.js
+++ script.js
@@ -3,30 +3,87 @@ var requestURL = "https://quiz-trainee.herokuapp.com/questions";
 var request = new XMLHttpRequest();
 request.open('GET', requestURL);
 var formulario;
-var aux=-1;
+var aux=-0;
 var NumeroDePerguntas;
 
 request.onreadystatechange = function(){
+
   if(request.readyState === 4 ){
     if(request.status === 200){
-      var formRadio = document.getElementsByName("resposta");
       formulario = JSON.parse(request.responseText);
-      for(var i=0; i<formulario[0];i++){
-        document.getElementById("titulo")=formulario.title;
-        }
-      }
     }
-  };
+  }
+};
+
+
+      
 
 
 request.send(null);
 function mostrarQuestao() {
- aux=aux+1;
+  
   document.getElementById("listaRespostas").style.display = "block";
   document.getElementById("confirmar").innerHTML = "Próxima";
   request.onreadystatechange;
   console.log(formulario);
+  console.log(aux);
 
+
+
+  var formRadio = document.getElementsByName("resposta");
+      
+      console.log(aux);
+      if(aux<0){
+        aux=aux+1;
+      }
+      else{
+        switch(aux){
+          case 0:
+            //console.log(aux + "deu bom")
+            document.getElementById("titulo").innerHTML=formulario[0].title;
+            for(var i = 0; i<4;i++){
+              document.getElementsByTagName("span")[i].innerHTML=formulario[0].options[i].answer
+              document.getElementsByTagName("input")[i].value=formulario[0].options[i].value
+            }
+            break;
+          case 1:
+            //console.log(aux + "deu bom")
+            document.getElementById("titulo").innerHTML=formulario[1].title;
+            for(var i = 0; i<4;i++){
+              document.getElementsByTagName("span")[i].innerHTML=formulario[1].options[i].answer
+              document.getElementsByTagName("input")[i].value=formulario[1].options[i].value
+              }
+            break;
+          case 2:
+            //console.log(aux + "deu bom")
+            document.getElementById("titulo").innerHTML=formulario[2].title;
+            for(var i = 0; i<4;i++){
+              document.getElementsByTagName("span")[i].innerHTML=formulario[2].options[i].answer
+              document.getElementsByTagName("input")[i].value=formulario[2].options[i].value
+              }
+            break;
+          case 3:
+            //console.log(aux + "deu bom")
+            document.getElementById("titulo").innerHTML=formulario[3].title;
+            for(var i = 0; i<4;i++){
+              document.getElementsByTagName("span")[i].innerHTML=formulario[3].options[i].answer
+              document.getElementsByTagName("input")[i].value=formulario[3].options[i].value
+              }
+            break;
+          case 4:
+            //console.log(aux + "deu bom")
+            document.getElementById("titulo").innerHTML=formulario[4].title;
+            for(var i = 0; i<4;i++){
+              document.getElementsByTagName("span")[i].innerHTML=formulario[4].options[i].answer
+              document.getElementsByTagName("input")[i].value=formulario[4].options[i].value
+              }
+            break;
+          default:
+              finalizarQuiz();
+              break;
+          }
+          aux=aux+1;
+        }
 
 
 }
