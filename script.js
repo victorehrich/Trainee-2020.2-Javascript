c2285365a85b4f4d872ae25458406cac6501b21e
Victor.Ehrich <victorehrich@gmail.com>
2021-01-22 14:59:22 -0300

    DEU BOM, FALTA SO COMENTAR

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var request = new XMLHttpRequest();
 request.open('GET', requestURL);
 
 var formulario;
-var aux=0;
+var aux=-1;
 
 request.onreadystatechange = function(){
 
@@ -14,41 +14,51 @@ request.onreadystatechange = function(){
     }
   }
 };
-var radioFalse = 0; //tentar fazer um true false pra libearr o botao
+var clicker = document.getElementById(("confirmar")); //tentar fazer um true false pra libearr o botao
 
 var valor = 0;
 request.send(null);
+
 function mostrarQuestao() {
   document.getElementById("resultado").innerHTML = "";
   document.getElementById("listaRespostas").style.display = "block";
-  document.getElementById("confirmar").innerHTML = "Próxima";
-  //console.log(formulario);
-  //console.log(aux);
+  //document.getElementById("confirmar").innerHTML = "Próxima";
 
-      if(aux<0){
-        aux=aux+1;
-      }
-      else if(aux < formulario.length){
-        //console.log(formulario.length);
-        //console.log(aux + "deu bom");
-        document.getElementById("titulo").innerHTML=formulario[aux].title;
-        for(var i = 0; i<formulario[aux].options.length;i++){
-          document.getElementsByTagName("span")[i].innerHTML=formulario[aux].options[i].answer
-          document.getElementsByTagName("input")[i].value=formulario[aux].options[i].value
-          //document.getElementsByTagName("input")[i].checked = false
-        }
 
-          for(var i = 0; i<formulario[aux].options.length;i++){
-            if(document.getElementsByTagName("input")[i].checked === true){
-              valor += parseInt(formulario[aux].options[i].value,10);
-              document.getElementsByTagName("input")[i].checked === false;
-              //console.log(document.getElementsByTagName("input")[i].value);
-              console.log(valor);
+      
+        if(aux === -1|| document.getElementsByTagName("input")[0].checked != false || document.getElementsByTagName("input")[1].checked != false || document.getElementsByTagName("input")[2].checked != false || document.getElementsByTagName("input")[3].checked != false ){ //Só permite que avance se uma resposta for marcada
+          if(aux != -1){
+            for(var i=0; i<formulario[aux].options.length; i++){ //contar os pontos
+                valor += document.getElementsByTagName("input")[i].checked*formulario[aux]['options'][i]['value']
             }
           }
-      aux=aux+1;
+          document.getElementById("confirmar").innerHTML = "Próxima" //"troca" o botão start pelo "Próxima"
+          aux++; //incrementa valor a questão para avançar
+        }
+
+        if (aux == -1) {//Remove as perguntas e repostas antes de iniciar o quiz
+          document.getElementById("titulo").classList.remove('hide');
+          document.getElementById("listaRespostas").remove('hide');
+      }
+  
+  
+
+        console.log(valor);
+        if(aux < formulario.length){
+        
+
+        document.getElementById("titulo").innerHTML=formulario[aux].title;
+        for(var i = 0; i<formulario[aux].options.length;i++){
+          if(document.getElementsByTagName("input")[i].checked === true){
+            document.getElementsByTagName("input")[i].checked = false
+          }
+          document.getElementsByTagName("span")[i].innerHTML=formulario[aux].options[i].answer
+          document.getElementsByTagName("input")[i].value=formulario[aux].options[i].value
+          
+        }
       }
       else{
+        console.log(formulario[aux-1].options.length)
         finalizarQuiz();
       }
       
@@ -56,7 +66,7 @@ function mostrarQuestao() {
 
 function finalizarQuiz() {
 
-  var max = 3*formulario[aux-1].options.length;
+  var max = 3*(formulario[aux-1].options.length+1);
   document.getElementById("listaRespostas").style.display = "none";
   document.getElementById("confirmar").innerHTML = "Refazer quiz";
 
@@ -64,7 +74,7 @@ function finalizarQuiz() {
 
   document.getElementById("resultado").innerHTML = "Sua pontuação: " + (valor*100/max) + "%";
 
-  aux = 0;
+  aux = -1;
   valor = 0;
 
 
