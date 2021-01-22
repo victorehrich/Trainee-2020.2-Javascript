5ebc3e9cf4ee1ef2cf813b297289df9d6a23b34f
Victor.Ehrich <victorehrich@gmail.com>
2021-01-22 01:19:20 -0300

    quase acabando

--- script.js
+++ script.js
@@ -1,93 +1,75 @@
+var formulario;
+var aux=0;
+var valor = 0;
 
+//requisicao ajax
 var requestURL = "https://quiz-trainee.herokuapp.com/questions";
 var request = new XMLHttpRequest();
 request.open('GET', requestURL);
-var formulario;
-var aux=-0;
-var NumeroDePerguntas;
 
+
+//parse do json para transformar os dados da API em um texto armazenado na variavel formulario
 request.onreadystatechange = function(){
-
-  if(request.readyState === 4 ){
+  if(request.readyState === 4){
     if(request.status === 200){
       formulario = JSON.parse(request.responseText);
     }
   }
 };
 
-
-      
-
-
 request.send(null);
+
 function mostrarQuestao() {
-  
+  document.getElementById("resultado").innerHTML = "";
   document.getElementById("listaRespostas").style.display = "block";
   document.getElementById("confirmar").innerHTML = "Próxima";
-  request.onreadystatechange;
-  console.log(formulario);
-  console.log(aux);
 
 
-
-  var formRadio = document.getElementsByName("resposta");
-      
-      console.log(aux);
       if(aux<0){
         aux=aux+1;
       }
-      else{
-        switch(aux){
-          case 0:
-            //console.log(aux + "deu bom")
-            document.getElementById("titulo").innerHTML=formulario[0].title;
-            for(var i = 0; i<4;i++){
-              document.getElementsByTagName("span")[i].innerHTML=formulario[0].options[i].answer
-              document.getElementsByTagName("input")[i].value=formulario[0].options[i].value
-            }
-            break;
-          case 1:
-            //console.log(aux + "deu bom")
-            document.getElementById("titulo").innerHTML=formulario[1].title;
-            for(var i = 0; i<4;i++){
-              document.getElementsByTagName("span")[i].innerHTML=formulario[1].options[i].answer
-              document.getElementsByTagName("input")[i].value=formulario[1].options[i].value
-              }
-            break;
-          case 2:
-            //console.log(aux + "deu bom")
-            document.getElementById("titulo").innerHTML=formulario[2].title;
-            for(var i = 0; i<4;i++){
-              document.getElementsByTagName("span")[i].innerHTML=formulario[2].options[i].answer
-              document.getElementsByTagName("input")[i].value=formulario[2].options[i].value
-              }
-            break;
-          case 3:
-            //console.log(aux + "deu bom")
-            document.getElementById("titulo").innerHTML=formulario[3].title;
-            for(var i = 0; i<4;i++){
-              document.getElementsByTagName("span")[i].innerHTML=formulario[3].options[i].answer
-              document.getElementsByTagName("input")[i].value=formulario[3].options[i].value
-              }
-            break;
-          case 4:
-            //console.log(aux + "deu bom")
-            document.getElementById("titulo").innerHTML=formulario[4].title;
-            for(var i = 0; i<4;i++){
-              document.getElementsByTagName("span")[i].innerHTML=formulario[4].options[i].answer
-              document.getElementsByTagName("input")[i].value=formulario[4].options[i].value
-              }
-            break;
-          default:
-              finalizarQuiz();
-              break;
-          }
-          aux=aux+1;
+      else if(aux < formulario.length){
+        document.getElementById("titulo").innerHTML=formulario[aux].title;
+        for(var i = 0; i<formulario[aux].options.length;i++){
+          document.getElementsByTagName("span")[i].innerHTML=formulario[aux].options[i].answer
+          document.getElementsByTagName("input")[i].value=formulario[aux].options[i].value
+          //document.getElementsByTagName("input")[i].checked = false
         }
+        
 
-
+          for(var i = 0; i<formulario[aux].options.length;i++){
+            if(document.getElementsByTagName("input")[i].checked === true){
+              document.getElementById("confirmar").disabled = false;
+              valor += parseInt(formulario[aux].options[i].value,10);
+              document.getElementsByTagName("input")[i].checked = false;
+              //console.log(document.getElementsByTagName("input")[i].value);
+              console.log(valor);
+            }
+            else{
+              document.getElementById("confirmar").disabled = true;
+            }
+          }
+      aux=aux+1;
+      }
+      else{
+        finalizarQuiz();
+      }
+      
 }
 
 function finalizarQuiz() {
-    
+
+  var max = 3*formulario[aux-1].options.length;
+  document.getElementById("listaRespostas").style.display = "none";
+  document.getElementById("confirmar").innerHTML = "Refazer quiz";
+
+  document.getElementById("titulo").innerHTML = "QUIZ DOS VALORES DA GTI";
+
+  document.getElementById("resultado").innerHTML = "Sua pontuação: " + (valor*100/max) + "%";
+
+  aux = 0;
+  valor = 0;
+
+
+  console.log(valor);
 }
