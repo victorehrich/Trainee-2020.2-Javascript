9d4032dd92cbb3076d430dbe199161810b76244f
Victor.Ehrich <victorehrich@gmail.com>
2021-01-22 02:33:57 -0300

    oi

--- script.js
+++ script.js
@@ -1,52 +1,49 @@
-var formulario;
-var aux=0;
-var valor = 0;
 
-//requisicao ajax
 var requestURL = "https://quiz-trainee.herokuapp.com/questions";
 var request = new XMLHttpRequest();
 request.open('GET', requestURL);
 
+var formulario;
+var aux=0;
 
-//parse do json para transformar os dados da API em um texto armazenado na variavel formulario
 request.onreadystatechange = function(){
+
   if(request.readyState === 4){
     if(request.status === 200){
       formulario = JSON.parse(request.responseText);
     }
   }
 };
+var radioFalse = 0; //tentar fazer um true false pra libearr o botao
 
+var valor = 0;
 request.send(null);
-
 function mostrarQuestao() {
   document.getElementById("resultado").innerHTML = "";
   document.getElementById("listaRespostas").style.display = "block";
   document.getElementById("confirmar").innerHTML = "Próxima";
-
+  //console.log(formulario);
+  //console.log(aux);
 
       if(aux<0){
         aux=aux+1;
       }
       else if(aux < formulario.length){
+        //console.log(formulario.length);
+        //console.log(aux + "deu bom");
         document.getElementById("titulo").innerHTML=formulario[aux].title;
         for(var i = 0; i<formulario[aux].options.length;i++){
           document.getElementsByTagName("span")[i].innerHTML=formulario[aux].options[i].answer
           document.getElementsByTagName("input")[i].value=formulario[aux].options[i].value
           //document.getElementsByTagName("input")[i].checked = false
         }
-        
 
           for(var i = 0; i<formulario[aux].options.length;i++){
             if(document.getElementsByTagName("input")[i].checked === true){
-              document.getElementById("confirmar").disabled = false;
               valor += parseInt(formulario[aux].options[i].value,10);
-              document.getElementsByTagName("input")[i].checked = false;
+              document.getElementsByTagName("input")[i].checked === false;
               //console.log(document.getElementsByTagName("input")[i].value);
               console.log(valor);
-            }
-            else{
-              document.getElementById("confirmar").disabled = true;
             }
           }
       aux=aux+1;
